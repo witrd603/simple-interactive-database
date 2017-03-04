368dc541d69022354ddb97f8a291c9dfebd18f52
ypegasi <ypegasi@hotmail.com>
2017-03-05 00:20:56 +0100

    simple Interactive database

---
+++ info-col.php
@@ -0,0 +1,15 @@
+<style type="text/css">
+#info-col {
+    position: absolute;
+    top:190px;
+    right:10px;
+    color:navy;
+    width:135px;
+    text-align:center;
+    margin: 5px 5px 0 0 ;
+}
+</style>
+<div id="info-col">
+<h3>This is the information column</h3>
+<p>Web design by <br>A W West</p>&nbsp;</p>
+
---
+++ template.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang=en>
+
+<head>
+    <title>Template for an interactive web page</title>
+    <meta charset=utf-8>
+    <link rel="stylesheet" type="text/css" href="includes.css">
+</head>
+
+<body>
+    <div id='çontainer'>
+        <?php include( 'header-for-template.php'); ?>
+        <?php include( 'nav.php'); ?>
+        <?php include( 'info-col.php'); ?>
+        <div id='çontent'>
+            <!--Start of page content.-->
+            <p> The home page content.
+
+            </p>
+            <!--End of the home page content.-->
+        </div>
+    </div>
+    <?php include( 'footer.php'); ?>
+</body>
+
+</html>
