2274b49ec7e4885e3cf68f5344cef4f8fa1b8951
ypegasi <ypegasi@hotmail.com>
2017-03-11 23:01:37 +0100

    add succesful section

--- register-page.php
+++ register-page.php
@@ -33,7 +33,34 @@
                               if (empty($POST['lname'])) {
                                   $errors[] ='You did not enter your last name.';
                               }
+                                    else {$ln =trim($_POST['lname']);
                           }
+                          //Was an email address entered?
+                                   if (empty($POST['email'])) {
+                                  $errors[] ='You did not enter your email address.';
+                                  }
+                                     else {$ln =trim($_POST['email']);
+                                     }
+                                     //Did the two passwords match?
+                                        if (!empty($POST['psword1'])) {
+                                            if ($_POST['psword1'] !=$POST['psword2'])  {
+                                                   $errors[] ='Your passwords where not the same.';
+                                            }
+                                                 else {$p =trim($_POST['psword1']);
+                                  }
+                                  }
+                                  //Start of the SUCCESFUL SECTION. i.e. all the fields were filled out
+                                  if (empty($errors)) {//if no problems encountered, register user in the database
+                                  require ('mysqli_connect.php');//Connect to the database.
+                                  //Make the query
+                                  $q = "INSERT INTO users (user_id, fname, lname, email, psword, registration_date)
+                                  VALUES (' ', '$fn', '$ln', '$e', '$SHA1('$p'), NOW() )";
+                                  $result= @mysqli_query($dbcon, $q); // Run the query.
+                                  if ($result) {// If it ran OK.
+                                  ...header ("location: register-thanks.php");
+                                  exit();
+                                  //End of SUCCESFUL SECTION}}
+
 
                           </p>
                       </div>
